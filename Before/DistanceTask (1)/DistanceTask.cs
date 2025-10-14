327a2b1885104e7061542435ae41842d857f97cd
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-14 20:12:35 +0500

    add cosinus in c# file

--- Before/DistanceTask (1)/DistanceTask.cs
+++ Before/DistanceTask (1)/DistanceTask.cs
@@ -7,20 +7,20 @@ public static class DistanceTask
     // Расстояние от точки (x, y) до отрезка AB с координатами A(ax, ay), B(bx, by)
     public static double GetDistanceToSegment(double ax, double ay, double bx, double by, double x, double y)
     {
-        // double DX = bx - ax;
-        // double DY = by - ay;
 
-        // double len = DX * DX + DY * DY;
-
-        // if (len != 0)
-        // {
-        //     if (dlina(defaultVector) * dlina(newVector) * )
-        // }
         double dlinaVectoraA = Math.Sqrt(((x - ax) * (x - ax)) + ((y - ay) * (y - ay)));
         double dlinaVectoraB = Math.Sqrt(((x - bx) * (x - bx)) + ((y - by) * (y - by)));
         double dlinaVectoraCOsnovanie = Math.Sqrt(((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)));
         double ifPerpendicular = 100000000;
-        if (dlinaVectoraCOsnovanie != 0)
+        var dxa = Math.Abs(x - ax);
+        var dya = Math.Abs(y - ay);
+        var dxb = Math.Abs(x - bx);
+        var dyb = Math.Abs(y - by);
+        var dxc = Math.Abs(ax - bx);
+        var dyc = Math.Abs(ay - by);
+        var cosOne = (dxa * dxc + dya * dyc) / (Math.Sqrt(dxa * dxa + dya * dya) + Math.Sqrt(dxc * dxc + dyc * dyc));
+        var cosTwo = (dxb * dxc + dyb * dyc) / (Math.Sqrt(dxb * dxb + dyb * dyb) + Math.Sqrt(dxc * dxc + dyc * dyc));
+        if (dlinaVectoraCOsnovanie != 0 && cosOne >= 0 && cosTwo >= 0)
         {
             double poluSquare = (dlinaVectoraA + dlinaVectoraB + dlinaVectoraCOsnovanie) / 2.0;
             double square = Math.Sqrt(poluSquare * (poluSquare - dlinaVectoraA) * (poluSquare - dlinaVectoraB) * (poluSquare - dlinaVectoraCOsnovanie));
