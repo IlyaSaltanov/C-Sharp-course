9e83b54aa07906df12f9548539ecc96ab969beff
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-15 21:01:52 +0500

    add Практика «Рубль -лей -ля»

---
+++ Модуль "Ветвления" /Практика «Рубль -лей -ля»/Практика «Рубль -лей -ля»/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+// Вставьте сюда финальное содержимое файла PluralizeTask.cs
+
+namespace Pluralize;
+
+public static class PluralizeTask
+{
+	public static string PluralizeRubles(int count)
+	{
+		// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+		if ((count % 100 >= 5 && count % 100 <= 20))
+			return "рублей";
+		else if ((count % 100 > 0) && (count % 10 == 1))
+			return "рубль";
+		else if (count % 10 >= 2 && count % 10 <= 4)
+			return "рубля";
+		return "рублей";
+	}
+}
