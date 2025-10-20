3f9b088e20557651d73ba919cf90db93076935d8
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-20 23:01:19 +0500

    add Четный массив

---
+++ Модуль "Массивы"/Четный массив/Четный массив/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+public static int MaxIndex(double[] array)
+{
+	var max = double.MinValue; 
+	int[] index = new 
+    foreach (var item in array)
+		if (item > max)
+		{
+			max = item;
+		}
+    return min;
+}
