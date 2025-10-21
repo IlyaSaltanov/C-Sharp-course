5a2ea5815000f612f0f365584b00b3fbfd030d2c
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-21 19:34:19 +0500

    add Поиск массива в массиве

---
+++ Модуль "Массивы"/Поиск массива в массиве/Program.cs
@@ -0,0 +1,15 @@
+﻿public static bool ContainsAtIndex(int[] sp, int[] subSp, int count)
+{
+	if (subSp.Length == 0)
+	{
+		return true;
+	}
+   // Сравниваем элементы посимвольно
+    for (int k = 0; k < subSp.Length; k++)
+    {
+        if (sp[count + k] != subSp[k])
+            return false;
+    }
+    
+    return true;
+}
