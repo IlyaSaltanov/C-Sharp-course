961ed5338789d7daa67eccb7eab17da3fcb11ebf
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-21 19:32:34 +0500

    add Подсчет

---
+++ Модуль "Массивы"/Подсчет/Program.cs
@@ -0,0 +1,15 @@
+﻿public static int GetElementCount(int[] items, int itemToCount)
+{
+	int k = 0;
+	if (items.Length > 0)
+	{	
+		for (int i = 0; i <= (items.Length-1); i++)
+		{
+			if (items[i] == itemToCount)
+			{
+				k = k + 1;
+			}
+		}
+	}
+	return k;
+}
