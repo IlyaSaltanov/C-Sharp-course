4501d858f69060d59c7cd78f65e9307323f212fc
IlyaSaltanov <mail@saltanov-pro.ru>
2025-10-21 19:29:46 +0500

    add public static int MaxIndex(double[] array) { 	if (array.Length == 0)         return -1; 	var max = array[0]; 	var index = 0; 	var list = new List<int>(){0};     for (int i = 1; i < array.Length; i++) 		if (array[i] > max) 		{ 			max = array[i]; 			index = i; 		}     return iclear

---
+++ Модуль "Массивы"/Индекс максимума/Program.cs
@@ -0,0 +1,15 @@
+﻿public static int MaxIndex(double[] array)
+{
+	if (array.Length == 0)
+        return -1;
+	var max = array[0]; 
+	var index = 0;
+	var list = new List<int>(){0};
+    for (int i = 1; i < array.Length; i++)
+		if (array[i] > max)
+		{
+			max = array[i];
+			index = i;
+		}
+    return index;
+}
