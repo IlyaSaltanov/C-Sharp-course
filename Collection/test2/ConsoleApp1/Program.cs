ea3f53b8237ce6200a8d03a8393c5ef2a49a6039
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-18 21:33:47 +0500

    добавляю задачи с семинара по C#

--- Collection/test2/ConsoleApp1/Program.cs
+++ Collection/test2/ConsoleApp1/Program.cs
@@ -1,5 +1,18 @@
 ﻿
 
-cd 
+class Program
+{
+    static int Sum(int[] list, int k, int s = 0)
+    {
+        if (k >= list.Length) return s;
+        return Sum(list, k+1, s + list[k]);
+        
+    }
+    static void Main(string[] args)
+    {
+        // Здесь будет ваш код
+        Console.WriteLine(Sum(new int[] { 1, 2, 4, 5 }, 0));
+    }
+}
 
 // Дополнительные элементы можно добавить здесь
