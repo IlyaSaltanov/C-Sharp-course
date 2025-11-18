4bd293fdc47962c07388133eac069920b96e38ea
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-18 21:33:32 +0500

    добавляю задачи с семинара по C#

---
+++ Recursions/StepenPracticum/Stepen2Practicum/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+
+class Program
+{
+    static int Stepen(int number)
+    {
+        return 0;
+    }
+    static int Alg(int number, int step)
+    {
+        int k = 1;
+        
+        for (int i = step; i >= 0; i--)
+        {
+            k = number * number;
+        }
+        return k;
+    }
+    static void Main(string[] args)
+    {
+        // Здесь будет ваш код
+        Console.WriteLine(Alg(5, 2));
+    }
+}
+
+// Дополнительные элементы можно добавить здесь
