9f7da38ae8a79f49aba8a56d5ca407e0f6f1c82e
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-19 11:30:34 +0500

    Brute force passwords

---
+++ Recursions/Brute force passwords/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        WriteAllWordsOfSize(1);
+        WriteAllWordsOfSize(2);
+        WriteAllWordsOfSize(0);
+        WriteAllWordsOfSize(4);
+    }
+
+    static void WriteAllWordsOfSize(int size)
+    {
+        MakeSubsets(new char[size]);
+    }
+
+    static void MakeSubsets(char[] subset, int position = 0)
+    {
+        if (position == subset.Length)
+        {
+            Console.WriteLine(new string(subset));
+            return;
+        }
+        subset[position] = 'a';
+        MakeSubsets(subset, position + 1);
+        subset[position] = 'b';
+        MakeSubsets(subset, position + 1);
+        subset[position] = 'c';
+        MakeSubsets(subset, position + 1);
+    }
+}
+
+// Дополнительные элементы можно добавить здесь
