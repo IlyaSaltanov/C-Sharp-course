dc894c68f96a15572608e694925a900342a60602
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-23 22:49:53 +0500

    solved route-planning

--- Recursions/route-planning (1)/PathFinderTask.cs
+++ Recursions/route-planning (1)/PathFinderTask.cs
@@ -1,12 +1,24 @@
 using System.Drawing;
+// using System.Collections.Generic;
+using System;
 
 namespace RoutePlanning;
 
 public static class PathFinderTask
 {
+	private static int[] bestOrder;
+    private static double bestLength;
+
 	public static int[] FindBestCheckpointsOrder(Point[] checkpoints)
 	{
-		var bestOrder = MakeTrivialPermutation(checkpoints.Length);
+		bestOrder = MakeTrivialPermutation(checkpoints.Length);
+		bestLength = checkpoints.GetPathLength(bestOrder);
+
+		var currentOrder = new int[checkpoints.Length];
+        currentOrder[0] = 0;
+
+		MakePermutations(checkpoints, currentOrder, 1, 0.0);
+
 		return bestOrder;
 	}
 
@@ -16,5 +28,29 @@ public static class PathFinderTask
 		for (var i = 0; i < bestOrder.Length; i++)
 			bestOrder[i] = i;
 		return bestOrder;
+	}
+
+	static void MakePermutations(Point[] checkpoints, int[] currentOrder, int position, double currentLength)
+	{
+		if (currentLength >= bestLength)
+			return;
+			
+		if (position == currentOrder.Length)
+		{
+      		bestLength = currentLength;
+            Array.Copy(currentOrder, bestOrder, currentOrder.Length);
+            return;
+		}
+
+		for (int i = 1; i < currentOrder.Length; i++)
+		{
+			var index = Array.IndexOf(currentOrder, i, 0, position);
+			if (index != -1)
+				continue;
+			currentOrder[position] = i;
+			double additionalLength = checkpoints[currentOrder[position - 1]].DistanceTo(checkpoints[i]);
+            double newLength = currentLength + additionalLength;
+			MakePermutations(checkpoints, currentOrder, position + 1, newLength);
+		}
 	}
 }
