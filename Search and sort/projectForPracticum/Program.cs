509135d40a24c1408c339ad0218472731b3e1260
IlyaSaltanov <mail@saltanov-pro.ru>
2025-11-25 18:34:49 +0500

    first

---
+++ Search and sort/projectForPracticum/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Здесь будет ваш код
+        int n = int.Parse(Console.ReadLine());
+        int[] sp = new int[n];
+        while (Console.ReadLine() != "q")
+        {
+            
+        }
+    }
+}
+
+// Дополнительные элементы можно добавить здесь
