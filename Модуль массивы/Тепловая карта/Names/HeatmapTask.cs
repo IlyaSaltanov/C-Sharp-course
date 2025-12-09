66a63099c9377a688a06a87d4e408dcf6af204f6
IlyaSaltanov <mail@saltanov-pro.ru>
2025-12-09 20:45:24 +0500

    сделал допольнительные фунции

--- Модуль массивы/Тепловая карта/Names/HeatmapTask.cs
+++ Модуль массивы/Тепловая карта/Names/HeatmapTask.cs
@@ -1,13 +1,24 @@
-﻿// Вставьте сюда финальное содержимое файла HeatmapTask.cs
-
-namespace Names;
+﻿namespace Names;
 
 internal static class HeatmapTask
 {
     public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
     {
-        var days = 30;
-        var months = 12;
+        var heat = FillHeatmapData(names);
+        var xTitle = GenerateDayTitle();
+        var yTitle = GenerateMonthTitle();
+        
+        return new HeatmapData(
+            "Тепловая карта рождаемости",
+            heat,
+            xTitle,
+            yTitle);
+    }
+
+    private static double[,] FillHeatmapData(NameData[] names)
+    {
+        const int days = 30;
+        const int months = 12;
         var heat = new double[days, months];
         
         foreach (var name in names)
@@ -18,25 +29,33 @@ internal static class HeatmapTask
             var month = name.BirthDate.Month;
             heat[day - 2, month - 1]++;
         }
+        
+        return heat;
+    }
 
-        var xTitle = new string[days];
-
+    private static string[] GenerateDayTitle()
+    {
+        var days = 30;
+        var labels = new string[days];
+        
         for (int q = 0; q < days; q++)
         {
-            xTitle[q] = (q + 2).ToString();
+            labels[q] = (q + 2).ToString();
         }
+        
+        return labels;
+    }
 
-        var yTitle = new string[months];
+    private static string[] GenerateMonthTitle()
+    {
+        var months = 12;
+        var labels = new string[months];
         
         for (int q = 0; q < months; q++)
         {
-            yTitle[q] = (q + 1).ToString();   
+            labels[q] = (q + 1).ToString();
         }
-
-        return new HeatmapData(
-            "Тепловая карта рождаемости",
-            heat,
-            xTitle,
-            yTitle);
+        
+        return labels;
     }
 }
--- Модуль массивы/Тепловая карта/Names/obj/Debug/net9.0/Names.AssemblyInfo.cs
+++ Модуль массивы/Тепловая карта/Names/obj/Debug/net9.0/Names.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Names")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccc8fc0552c462ac3d9295be46b2b9c6e9dac7cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2136955ac0fb684fa7f8fd0bb2f54ff7cda57bc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Names")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Names")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
