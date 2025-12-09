2136955ac0fb684fa7f8fd0bb2f54ff7cda57bc9
IlyaSaltanov <mail@saltanov-pro.ru>
2025-12-09 20:37:17 +0500

    решил задачу тепловая карта

---
+++ Модуль массивы/Тепловая карта/Names/HeatmapTask.cs
@@ -0,0 +1,42 @@
+﻿// Вставьте сюда финальное содержимое файла HeatmapTask.cs
+
+namespace Names;
+
+internal static class HeatmapTask
+{
+    public static HeatmapData GetBirthsPerDateHeatmap(NameData[] names)
+    {
+        var days = 30;
+        var months = 12;
+        var heat = new double[days, months];
+        
+        foreach (var name in names)
+        {
+            var day = name.BirthDate.Day;
+            if (day == 1) continue;
+            
+            var month = name.BirthDate.Month;
+            heat[day - 2, month - 1]++;
+        }
+
+        var xTitle = new string[days];
+
+        for (int q = 0; q < days; q++)
+        {
+            xTitle[q] = (q + 2).ToString();
+        }
+
+        var yTitle = new string[months];
+        
+        for (int q = 0; q < months; q++)
+        {
+            yTitle[q] = (q + 1).ToString();   
+        }
+
+        return new HeatmapData(
+            "Тепловая карта рождаемости",
+            heat,
+            xTitle,
+            yTitle);
+    }
+}
---
+++ Модуль массивы/Тепловая карта/Names/HistogramTask.cs
@@ -0,0 +1,21 @@
+﻿// Вставьте сюда финальное содержимое файла HistogramTask.cs
+
+namespace Names;
+
+internal static class HistogramTask
+{
+    public static HistogramData GetBirthsPerDayHistogram(NameData[] names, string name)
+    {
+        var days = new string[31];
+		for (var y = 0; y < days.Length; y++)
+			days[y] = (y + 1).ToString();
+		var birthsCounts = new double[31];
+		foreach (var nameTwo in names)
+            if (nameTwo.Name == name && nameTwo.BirthDate.Day != 1)
+            {
+                birthsCounts[nameTwo.BirthDate.Day-1]++;
+            }
+        return new HistogramData(
+            $"Рождаемость людей с именем '{name}'", days, birthsCounts);
+    }
+}
