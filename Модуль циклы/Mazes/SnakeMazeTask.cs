eac41aea13e13dc7fef83b2d495d1d6a33250ddd
IlyaSaltanov <mail@saltanov-pro.ru>
2025-12-10 20:19:33 +0500

    обновил Практика «Лабиринт змейка»

--- Модуль циклы/Mazes/SnakeMazeTask.cs
+++ Модуль циклы/Mazes/SnakeMazeTask.cs
@@ -3,34 +3,17 @@
 namespace Mazes;
 
 public static class SnakeMazeTask
-{
-    public static void DoStepFromRightToLeft(Robot robot, int width, Direction dir)
-    {
-        for (int q = 0; q < width - 2 - 1; q++)
-        {
-            robot.MoveTo(dir);   
-        }
-    }
-
-    public static void Turning(Robot robot, int width, int height)
-    {
-
-        robot.MoveTo(Direction.Down);
-		robot.MoveTo(Direction.Down); 
-
-        DoStepFromRightToLeft(robot, width, Direction.Left);
-    }
-
+{   
     public static void LookingForWayOut(Robot robot, int width, int height)
     {
         int LengthWey = (height - 1 - 1) / 2 + 1;
         for (int q = 0; q < LengthWey; q++)
         {
-            if (q % 2 == 0)
+            if ((q % 2) == 0)
             {
                 DoStepFromRightToLeft(robot, width, Direction.Right);   
             }
-            else if (q % 2 == 1 && LengthWey - q > 1)
+            else if ((LengthWey - q > 1) && (q % 2 == 1))
             {
 				Turning(robot, width, height);
 
@@ -41,6 +24,23 @@ public static class SnakeMazeTask
             {
                 Turning(robot, width, height);   
             }
+        }
+    }
+
+    public static void Turning(Robot robot, int width, int height)
+    {
+
+        robot.MoveTo(Direction.Down);
+        robot.MoveTo(Direction.Down);
+
+        DoStepFromRightToLeft(robot, width, Direction.Left);
+    }
+    
+    public static void DoStepFromRightToLeft(Robot robot, int width, Direction dir)
+    {
+        for (int q = 0; q < width - 2 - 1; q++)
+        {
+            robot.MoveTo(dir);
         }
     }
 }
